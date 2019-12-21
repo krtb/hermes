c4bd0aaa75fb1fa5c600e612e04dddd66401a94d
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-21 18:54:15 -0500

    require passport, instantiate GoogleStrategy

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 const app = express()
 
+passport.use(new GoogleStrategy());
 
 const PORT = process.env.PORT || 5000
 console.log(`App is running on port ${PORT}`)
