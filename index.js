2301b519394f5013ed4dc57be790e9bd97877622
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-21 18:38:47 -0500

    remove get request used for testing

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req,res) => {
-    res.send({hi: 'there'})
-})
 
 const PORT = process.env.PORT || 5000
 console.log(`App is running on port ${PORT}`)
