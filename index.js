220d2d593a023f80cd73ecf9b12b3ab68368b743
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-26 17:14:30 -0500

    pass in clientID,Secrete,URL, log accessToken

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const app = express()
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: 'auths/google/callback'
+    },
+    (accessToken) => {
+        console.log(accessToken);
+    }
+));
 
 const PORT = process.env.PORT || 5000
 
