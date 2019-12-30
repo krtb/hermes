c389dccc57286e256bbfa19117b008a92a90476f
Kurt Bauer <kurtbauer55@protonmail.com>
2019-12-29 23:13:35 -0500

    google strategy fully set up, for passport

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ passport.use(new GoogleStrategy(
     {
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
-        callbackURL: 'auths/google/callback'
+        callbackURL: 'http://localhost:5000/auth/google/callback'
     },
-    (accessToken) => {
-        console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+
+        console.log('ACCESS TOKEN', accessToken);
+        console.log('REFRESH TOKEN', refreshToken);
+        console.log('profile:', profile);
+
+        done(null, profile);
     }
 ));
 
