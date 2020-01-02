c3b31eac91aeadda088d7ce37d17b7cf1999155f
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-01 22:26:40 -0500

    add GoogleStrategy to passport.js, require module

---
+++ services/passport.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: 'http://localhost:5000/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+
+        console.log('ACCESS TOKEN', accessToken);
+        console.log('REFRESH TOKEN', refreshToken);
+        console.log('profile:', profile);
+
+        done(null, profile);
+    }
+));
