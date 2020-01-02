4ad37195431033b83b8b5c0f8b7226e3dabc62e6
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-01 22:23:58 -0500

    remove passport googleStrategy from index.js

--- index.js
+++ index.js
@@ -3,22 +3,6 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const app = express()
 
-passport.use(new GoogleStrategy(
-    {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: 'http://localhost:5000/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-
-        console.log('ACCESS TOKEN', accessToken);
-        console.log('REFRESH TOKEN', refreshToken);
-        console.log('profile:', profile);
-
-        done(null, profile);
-    }
-));
-
 const PORT = process.env.PORT || 5000
 
 console.log(`App is running on port ${PORT}`)
