a66824c3fec1629dfb1633934d72dda399b16396
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-03 12:48:40 -0500

    require mongoose, cookieSession, passport in index

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 require('./services/passport')
 
 const app = express()
