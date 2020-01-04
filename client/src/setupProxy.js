68edbf6084f5addbc31fc8a612d23713e26d331e
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-04 18:06:11 -0500

    set up proxy, fix create-react-app3.0 bug

---
+++ client/src/setupProxy.js
@@ -0,0 +1,14 @@
+// FIX FOR (create-react-app3.0) BUG
+// would see the following errors:
+
+// [1] When specified, "proxy" in package.json must be a string.
+
+// [1] Instead, the type of "proxy" was "object".
+
+// [1] Either remove "proxy" from package.json, or make it a string.
+
+const proxy = require('http-proxy-middleware')
+
+module.exports = function (app) {
+    app.use(proxy(['/api', '/auth/google'], { target: 'http://localhost:5000' }));
+}
