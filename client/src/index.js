c994317d753415fc451df81297327b3471cd0f2c
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-06 08:33:23 -0500

    import provider, createStore, applyMiddleware

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-redux';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
