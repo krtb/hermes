47818515f4757938c8e5ac02cbb3d3f57d0439d9
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-07 10:57:01 -0500

    add dumby components to check react-router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Router } from 'react-router-dom'
+
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
  
 const App = () => {
     return(
         <div>
-            Hello App Component
+            <BrowserRouter>
+                <Route exact={true} path="/" component={Landing} />
+
+                <Route exact path="/surveys" component={Dashboard} />
+
+                <Route path="/surveys/new" component={SurveyNew} />
+            </BrowserRouter>
         </div>
     );
 }
