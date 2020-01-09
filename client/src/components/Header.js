5a919599d04440b267c9eb525468befff017f229
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-09 13:55:35 -0500

    programatically render login button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,13 +4,25 @@ import { connect } from 'react-redux';
 
 class Header extends Component {
 
+    // conditionally render based on state from authReducer & fetchUser action
+    renderContent = () => {
+        switch (this.props.auth) {
+            case null:
+                return 'Loading...';
+            case false:
+                return <li> <a href="/auth/google" > Login with Google </a> </li>;
+            default:
+                return <li> <a href="/api/logout" > Log out </a> </li>;
+        }
+    }
+
     render() {
         return(
             <nav>
                 <div class="nav-wrapper">
                     <a href="#" class="brand-logo">HERMES</a>
                     <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li><a>Log In</a></li>
+                        { this.renderContent() }
                     </ul>
                 </div>
             </nav>
