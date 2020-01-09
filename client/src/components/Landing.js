d47c54bcbcf6cb47e5770b47c328255674101029
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-09 13:48:21 -0500

    add landing component

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Landing = () => {
     return (
-        <div>
-            LANDING COMPONENT
+        <div style={{textAlign: 'center'}} >
+            <h1>
+                Hermes
+            </h1>
+            An app to collect new job postings and send them to you in an email
         </div>
     )
 }
