ede5e81bc2e9ae414f710154c2d8a54fafef2426
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-09 13:36:11 -0500

    add materialize component, connect mapStateToProps

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 
 class Header extends Component {
+
     render() {
         return(
-            <div>
-                Header Component
-            </div>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">Logo</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="collapsible.html">JavaScript</a></li>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
 
-export default Header;
+function mapStateToProps ({auth}) {
+    return {
+        auth
+    }
+}
+
+export default connect(mapStateToProps)(Header);
