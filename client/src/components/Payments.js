72da4d1d308c5919bd7112e50798a0a278d98776
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-12 17:33:21 -0500

    add StripeCheckout element, with button

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
 import StripeCheckout from 'react-stripe-checkout';
 import { connect } from 'react-redux';
+import * as actions from '../actions'; // import actions to use handleToken action creator
 
 class Payments extends Component {
     render(){
         return(
             <div>
-                Payments Component
+                <StripeCheckout
+                    name="Hermes"
+                    description= "$5 for 5 email credits"
+                    amount={500}
+                    token = {token => this.props.handleToken(token)}
+                    stripeKey={process.env.REACT_APP_STRIPE_KEY}
+                >
+                    <button className="btn" >
+                        Add Credits
+                    </button>
+                </StripeCheckout>
             </div>
         )
     } 
 }
 
-export default connect()(Payments);
+export default connect(null, actions)(Payments);
