1e6195abfa1b45520baf8b95ed6245cb7abf7a2d
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-13 13:32:20 -0500

    render Stripe payments component, and user credits

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-
+import Payments from './Payments';
 class Header extends Component {
 
     // conditionally render based on state from authReducer & fetchUser action
@@ -12,7 +12,11 @@ class Header extends Component {
             case false:
                 return <li> <a href="/auth/google" > Login with Google </a> </li>;
             default:
-                return <li> <a href="/api/logout" > Log out </a> </li>;
+                return [
+                    <li key="1" ><Payments /> </li>,
+                    <li style={{ margin: '0 10px' }} > Credits: {this.props.auth} </li>,
+                    <li key="2" > <a href="/api/logout" > Log out </a> </li>
+                ];
         }
     }
 
