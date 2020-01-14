0b2c5819681df0b4bd6de48be86de0de36a1bbc6
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-14 12:37:33 -0500

    add inline descriptions of action creators

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import {FETCH_USER }from './types';
 
+// (purpose): fetch User profile from Express/Node API backend
 export const fetchUser = () => async dispatch => {
         const res = await axios.get('/api/current_user')
         dispatch({ type: FETCH_USER, payload: res.data })
 }
 
+// (purpose): handle token sent from Stripe to identify user
 export const handleToken = () => async dispatch => {
         const res = await axios.post('api/stripe', token)
         dispatch({type: FETCH_USER, payload: res.data})
