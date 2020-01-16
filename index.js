a7409df511adf45ed73a1c40493fef43d5e1203a
Kurt Bauer <kurtbauer55@protonmail.com>
2020-01-16 10:08:08 -0500

    configure Express routing for production

--- index.js
+++ index.js
@@ -25,6 +25,19 @@ app.use(passport.session())
 require('./routes/authRoutes')(app)
 require('./routes/billingRoutes')(app);
 
+// configuration for Express, ONLY when in production environment
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like main.js or main.css files
+    app.use(express.static('client/build'));
+    // Express will sever up the index.html file
+    // if it doesn't recognize the route.
+    const path = require('path');
+    app.get('*', (req, res) => {
+        path.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 const PORT = process.env.PORT || 5000
 console.log(`App is running on port ${PORT}`)
 app.listen(PORT)
